9ee24241889cf6f5bcefecc71d6e52081d8968e6
VaitoSoi <92448929+VaitoSoi@users.noreply.github.com>
2022-07-02 17:05:15 +0700

    Update statusArray_2.js

--- info/statusArray_2.js
+++ info/statusArray_2.js
@@ -1,6 +1,4 @@
 module.exports = [
-    'Rip OggyTheBot 2#8728 (14/5/2022 - 16/6/2022).',
-    'Đóng cửa sau 2 ngày.',
-    'Cảm ơn 110 servers và 4300 members.',
-    'Rip 2y2c.org :(('
+    'Online angain :))',
+    'Không có gì đâu đừng có mà soi :v'
 ]
